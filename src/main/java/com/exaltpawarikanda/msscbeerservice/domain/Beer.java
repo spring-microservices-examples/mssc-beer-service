633a3a24e4a812a1d20bb01e03bb0b61e2420e75
exalt <exaltpawa@gmail.com>
2021-07-29 00:02:31 +0200

    Added Validation to the Beer Dto

--- src/main/java/com/exaltpawarikanda/msscbeerservice/domain/Beer.java
+++ src/main/java/com/exaltpawarikanda/msscbeerservice/domain/Beer.java
@@ -2,15 +2,10 @@ package com.exaltpawarikanda.msscbeerservice.domain;
 
 import com.exaltpawarikanda.msscbeerservice.enums.BeerStyle;
 import lombok.*;
-import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import java.math.BigDecimal;
-import java.time.OffsetDateTime;
-import java.util.UUID;
 
 /**
  * Created by Exalt Pawarikanda on 7/27/21
