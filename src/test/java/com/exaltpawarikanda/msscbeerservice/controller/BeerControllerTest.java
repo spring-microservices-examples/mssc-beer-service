4fcda9ae446b200c68dc6114d41411a44141765c
exalt <exaltpawa@gmail.com>
2021-07-31 00:50:56 +0200

    Added fix to failing tests

--- src/test/java/com/exaltpawarikanda/msscbeerservice/controller/BeerControllerTest.java
+++ src/test/java/com/exaltpawarikanda/msscbeerservice/controller/BeerControllerTest.java
@@ -1,5 +1,7 @@
 package com.exaltpawarikanda.msscbeerservice.controller;
 
+import com.exaltpawarikanda.msscbeerservice.domain.Beer;
+import com.exaltpawarikanda.msscbeerservice.enums.BeerStyle;
 import com.exaltpawarikanda.msscbeerservice.model.BeerDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -8,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -35,7 +38,7 @@ class BeerControllerTest {
     @Test
     void createBeer() throws Exception {
 
-        BeerDto beerDto = BeerDto.builder().build();
+        BeerDto beerDto = getValidBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
         mockMvc.perform(post("/api/v1/beer")
@@ -46,7 +49,7 @@ class BeerControllerTest {
 
     @Test
     void updateBeerById() throws Exception{
-        BeerDto beerDto = BeerDto.builder().build();
+        BeerDto beerDto = getValidBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
         mockMvc.perform(put("/api/v1/beer/" + UUID.randomUUID().toString())
                 .contentType(MediaType.APPLICATION_JSON)
@@ -59,5 +62,14 @@ class BeerControllerTest {
         mockMvc.perform(delete("/api/v1/beer/" + UUID.randomUUID().toString())
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNoContent());
+    }
+
+    BeerDto getValidBeerDto(){
+        return BeerDto.builder()
+                .beerName("Castle")
+                .beerStyle(BeerStyle.LAGER)
+                .upc(613973343411L)
+                .price(new BigDecimal("2.99"))
+                .build();
     }
 }
