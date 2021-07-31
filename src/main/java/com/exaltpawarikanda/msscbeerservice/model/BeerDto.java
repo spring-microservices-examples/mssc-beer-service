372d2d0d63bcd456373953f76c9159aa6baffd85
exalt <exaltpawa@gmail.com>
2021-08-01 01:14:32 +0200

    Added Jackson Config for BeerDto

--- src/main/java/com/exaltpawarikanda/msscbeerservice/model/BeerDto.java
+++ src/main/java/com/exaltpawarikanda/msscbeerservice/model/BeerDto.java
@@ -1,6 +1,9 @@
 package com.exaltpawarikanda.msscbeerservice.model;
 
 import com.exaltpawarikanda.msscbeerservice.enums.BeerStyle;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,18 +26,26 @@ import java.util.UUID;
 @Data
 public class BeerDto {
 
- /*   @Null
+    @JsonIgnore
+    @Null
     private UUID id;
+
+    @JsonIgnore
     @Null
     private Long version;
+
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd",shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdAt;
+
+    @JsonIgnore
     @Null
-    private OffsetDateTime lastUpdatedAt;*/
+    private OffsetDateTime lastUpdatedAt;
 
     @NotBlank
     private String beerName;
 
+    @JsonProperty("beer-style")
     @NotNull
     private BeerStyle beerStyle;
 
@@ -42,6 +53,7 @@ public class BeerDto {
     @NotNull
     private Long upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
